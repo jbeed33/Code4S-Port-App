d572774d6f9aa434db89d9896dd4eceafe0fdc3a
Joshua Beed <jbeed33@gmail.com>
2023-11-29 23:54:01 -0800

    updated expand function to expand nodes. Not tested yet

--- expand.hpp
+++ expand.hpp
@@ -36,36 +36,74 @@ vector<int> getWallsForZone(Node &n, int zone ){
 
 }
 
-void getContainerDataFromCranePos(Node& n, bool &hasBox, vector<vector<int>>& boxPos, Container box){
-    if(n.craneLocation == 0){ //crane starts from ship
+vector<vector<int>> getContainerDataFromCranePos(Node& n, int craneCol, int zone){
+
+    vector<vector<int>> boxPos;
+
+        if(zone == 0){ //crane starts from ship
             for (int j = 0; j < n.ship.size(); j++) 
 		    { 
-                if(n.ship.at(j).at(n.cranePos.first).status != 0){
-                    boxPos.push_back({j, n.cranePos.first});
-                    box = n.ship.at(j).at(n.cranePos.first);
+                if(n.ship.at(j).at(craneCol).status != 0){
+                    boxPos.push_back({j, craneCol});
                     break;
                 }
 			
 		    }
         }
-        else if(n.craneLocation == 2){ //crane starts from buffer
+        else if(zone == 2){ //crane starts from buffer
             for (int j = 0; j < n.buffer.size(); j++) 
 		    { 
-                if(n.buffer.at(j).at(n.cranePos.first).status != 0){
-                    hasBox = true;
-                    boxPos.push_back({j, n.cranePos.first});
-                    box = n.buffer.at(j).at(n.cranePos.first);
+                if(n.buffer.at(j).at(craneCol).status != 0){
+                    boxPos.push_back({j, craneCol});
                     break;
                 }
 			
 		    }
         }
-        else if(n.craneLocation == 1){ // crane starts from truck
+        else if(zone == 1){ // crane starts from truck
             // take from loading buffer if there is set hasBox to true
         }
 }
 
- // left / right for crane ( store vector< endCol, endZone>)
+int FindEmptySpotInCol(Node &n, int col, int zone){
+    
+    if(zone == 0){ //ship
+		for (int j = 0; j < n.ship.size(); j++) 
+		{ 
+		    if(n.ship.at(j).at(col).status != 0){
+                
+                if(j != 0){ // not the top row
+                  return j-1;  
+                }
+                break;
+            } else{
+                if(j == n.ship.size()-1){ // bottom row
+                    return j;
+                }
+            }
+			
+		}	 
+    }
+    else if( zone == 2){ //buffer
+        for (int j = 0; j < n.buffer.size(); j++) 
+		{ 
+		    if(n.buffer.at(j).at(col).status != 0){
+                
+                if(j != 0){ // not the top row
+                  return j-1;  
+                }
+                break;
+            } else{
+                if(j == n.buffer.size()-1){ // bottom row
+                    return j;
+                }
+            }
+			
+		}
+    }
+		 
+}
+ // left / right for crane ( store vector< endRow, endCol, endZone>)
 vector<vector<int>> craneMovement(Node &n, vector<int> avaliableColsForBuffer, vector<int> avaliableColsForShip ){
             vector<vector<int>> craneMovement;
 
@@ -176,16 +214,74 @@ vector<vector<int>> craneMovement(Node &n, vector<int> avaliableColsForBuffer, v
         }
 }
 
+void swapContainers(Node &n, int prevX, int prevY, int newX, int newY, int startZone, int endZone){
+    
+    
+    Container startContainer;
+    Container endContainer;
+
+    
+    if(startZone == 0){
+        startContainer = n.ship.at(prevX).at(prevY);
+    }
+    else if(startZone == 2){
+        startContainer = n.buffer.at(prevX).at(prevY);
+    }
+    else if(startZone == 1){
+        //truck
+    }
+
+     if(endZone == 0){
+        endContainer = n.ship.at(newX).at(newY);
+    }
+    else if(endZone == 2){
+        endContainer = n.buffer.at(newX).at(newY);
+    }
+    else if(endZone == 1){
+        //truck
+    }
+
+    //place in correct Zones. The boxes swap places!!!
+     if(startZone == 0){
+        n.ship.at(prevX).at(prevY) = endContainer;
+    }
+    else if(startZone == 2){
+        n.buffer.at(prevX).at(prevY) = endContainer;
+    }
+    else if(startZone == 1){
+        //truck
+    }
+
+     if(endZone == 0){
+         n.ship.at(newX).at(newY) = startContainer;
+    }
+    else if(endZone == 2){
+        n.buffer.at(newX).at(newY) = startContainer;
+    }
+    else if(endZone == 1){
+        //truck
+    }
+
+
+    
+
+
+}
+
 vector<Node> expandNode(Node &n, vector<vector<int>> boxPos, bool hasBox, vector<vector<int>> craneMoves  ){
 
+    //call cost function
+    //call hurestic function
+    
     vector<Node> expandedNodesList;
 
     //Creating nodes (expand)
     for(int i = 0; craneMoves.size(); i++){
         //Check to go
+
         //just crane
         Node newNode = n;
-        newNode.cranePos = {craneMoves.at(i).at(0), n.cranePos.second};
+        newNode.cranePos = {craneMoves.at(i).at(0), n.cranePos.second}; //(x,y)
         newNode.craneLocation = craneMoves.at(i).at(1);
 
         expandedNodesList.push_back(newNode);
@@ -194,27 +290,19 @@ vector<Node> expandNode(Node &n, vector<vector<int>> boxPos, bool hasBox, vector
         //Check to see if I am unloading a container that cannot be unloaded
         
         if(hasBox){
-            Node newNodewithBox = newNode;
+            Node newNodewithBox = n;
           
-            //update buffer or ship truck
-            if(n.craneLocation = 0){ //ship
+            //update buffer, ship, or truck
 
                 int oldx = boxPos.at(0).at(0);
-                int oldy = boxPos.at(0).at(1); 
-                n.ship.at(oldx).at(oldy) = {"", 0.0, 0}; // empty container 
+                int oldy = boxPos.at(0).at(1);
+                int newx = craneMoves.at(i).at(0);
+                int newy = FindEmptySpotInCol(n, newx, craneMoves.at(i).at(1)); 
+            
+                swapContainers(newNodewithBox, oldx, oldy, newx, newy, n.craneLocation, craneMoves.at(i).at(1));
 
-                
-
-            } else if(n.craneLocation == 1 ){
-                // need to know what the storage of a truck looks like just an array of size one or a vector ?
-            } else if(n.craneLocation == 2){
-
-            }
-            // update crane location to ending position
-            //update buffer, ship, or truck new positon
-            // return the new Node
+                expandedNodesList.push_back(newNodewithBox);
         }
-    }
 
     return expandedNodesList;
 }
@@ -225,8 +313,6 @@ vector<Node> expandNodeBasedOnCraneMovement(Node n){
     // 0 - ship
     // 1 - truck
     // 2 - buffer
-    
-    vector<Node> returnedStates;
 
     vector<int> avaliableColsForBuffer = getWallsForZone(n, 1);
     vector<int> avaliableColsForShip = getWallsForZone(n, 0);
@@ -237,21 +323,28 @@ vector<Node> expandNodeBasedOnCraneMovement(Node n){
     bool hasBox = false;
     vector<vector<int>> boxPos;
     Container box;
+    int craneCol = n.cranePos.first;
+    int zone = n.craneLocation;
+    boxPos = getContainerDataFromCranePos(n,craneCol, zone );
 
-    getContainerDataFromCranePos(n, hasBox, boxPos, box);
+    if(boxPos.size() > 0){
+        hasBox = true;
+
+        if(zone == 0){ //ship
+           box = n.ship.at(boxPos.at(0).at(0)).at(boxPos.at(0).at(1));
+        }
+        else if(zone == 2){ //buffer
+          box = n.buffer.at(boxPos.at(0).at(0)).at(boxPos.at(0).at(1));
+        }
+    }
         
 
-    //Crane movemnt
+    //Crane movemnt 
      vector<vector<int>> craneMoves = craneMovement(n, avaliableColsForBuffer, avaliableColsForShip );
 
 
     //Expand Nodes
     return expandNode(n, boxPos, hasBox, craneMoves);
-
-    
-    //call cost function
-    //call hurestic function
-    
     
     
     //if at the truck goes left buffer, right is the ship
@@ -261,9 +354,4 @@ vector<Node> expandNodeBasedOnCraneMovement(Node n){
     // with: crane startCol boxes -> magic -> node -> update ship / buffer as need
     // if the box, unloadable -> truck -> update ship / buffer
 
-
-    return returnedStates;
 }
-
-
-vector<vector<int>> containersToBalance(Node n){
