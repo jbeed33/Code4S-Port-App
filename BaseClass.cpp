9bfbb4ac881c355f0330ca6471f5c8ac55cf236a
Phoenix Winter <phoenixwinter@gmail.com>
2023-12-11 15:06:04 -0800

    Updated cost function to account for new expand nodes

--- BaseClass.cpp
+++ BaseClass.cpp
@@ -44,61 +44,24 @@ Node Base::search(vector<vector<tuple<Container, int>>> ship, int craneRow, int 
 *	takes the node we are expanding from, the start and end columns, the start and end zones,
 *	and if the crane is carrying a container for the current move
 */
-int Base::cost(Node current, int startCol, int endCol, int startZone, int endZone, bool justCrane){
+int Base::cost(Node current){
 	int cost = 0;
-	int startTop;
-	int endTop;
-	int startZoneHeight;
-	int endZoneHeight;
 
-	int craneRow = current.cranePos.first;
-	int craneCol = current.cranePos.second;
-
-	if(0 == startZone){	//Starts on ship
-		startTop = findTopShip(current.ship, startCol);
-		startZoneHeight = SHIPHEIGHT - 1;
-	}
-	else if(2 == startZone){	//starts in buffer
-		startTop = findTopBuffer(current.buffer, startCol);
-		startZoneHeight = BUFFERHEIGHT - 1;
-	}
-	else{	//Starts on truck
-		startTop = 0;
-		startZoneHeight = 0;
-	}
-
-	if(0 == endZone){	//Ends on ship
-		endTop = 1 + findTopShip(current.ship, endCol);
-		endZoneHeight = SHIPHEIGHT - 1;
-	}
-	else if(2 == endZone){	//Ends in buffer
-		endTop = 1 + findTopShip(current.buffer, endCol);
-		endZoneHeight = BUFFERHEIGHT - 1;
-	}
-	else{	//Ends on truck
-		endTop = 0;
-		endZoneHeight = 0;
-	}
-
-	if(true == justCrane && 0 == current.prev[2][0]){	//Dropped off a container to start move
-		cost += craneRow - (startTop + 1);
-	}
-	else if(false == justCrane && 1 == current.prev[2][0]) {	//Picked up a container to start move
-		cost += craneRow - startTop;
-	}
-	else{	//Maintain position to start move
-		startTop = craneRow;
-	}
+	int startRow = current.prev[0][0];
+	int startCol = current.prev[0][1];
+	int startZone = current.prev[0][2];
+	int endRow = current.prev[1][0];
+	int endCol = current.prev[1][1];
+	int endZone = current.prev[1][2];
 
 	if(startZone != endZone){	//move between zones
 		cost += PORTALTIME;
-		cost += startZoneHeight - startTop;	//move up to portal
 	}
 	else{
-		cost += SINGLEMOVETIME;
-		if(endTop > startTop){	//have to move up to get over containers
-			cost += endTop - startTop;
-		}
+		cost += max(startCol, endCol) - min(startCol, endCol);
 	}
+
+	cost += max(startRow, endRow) - min(startRow, endRow);
+
 	return cost;
 }
